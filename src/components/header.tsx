bc1f1e65a87bfad0784ea50873fee54b2252b22e
Limofeus <69484438+Limofeus@users.noreply.github.com>
2026-06-15 22:59:52 +0300

    Update header component title and SVG paths

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+// src/components/header.tsx
 import { isTauriAndroid } from "@/lib/platform";
 import { ThemeToggle } from "./mode-toggle";
 
@@ -19,13 +20,14 @@ const Header = () => {
                 strokeLinejoin="round"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M15 8h.01" />
-                <path d="M11 21h-5a3 3 0 0 1 -3 -3v-12a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v5.5" />
-                <path d="M3 16l5 -5c.928 -.893 2.072 -.893 3 0l2 2" />
-                <path d="M17.8 20.817l-2.172 1.138a.392 .392 0 0 1 -.568 -.41l.415 -2.411l-1.757 -1.707a.389 .389 0 0 1 .217 -.665l2.428 -.352l1.086 -2.193a.392 .392 0 0 1 .702 0l1.086 2.193l2.428 .352a.39 .39 0 0 1 .217 .665l-1.757 1.707l.414 2.41a.39 .39 0 0 1 -.567 .411l-2.172 -1.138z" />
+                <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+                <path d="M9 12h6" />
+                <path d="M9 15h6" />
+                <path d="M9 18h2" />
               </svg>
               <span className="text-foreground text-lg font-bold sm:inline-block">
-                Image Compressor
+                ВОРДКОМПРЕСОР3000
               </span>
             </a>
           </div>
